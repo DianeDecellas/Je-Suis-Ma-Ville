6960aca7705e9554d5c0bbe2528b1ffe16602189
GuillaumeCx <guillaume.caucheteux@hotmail.fr>
2021-04-07 14:34:49 +0200

    Exemple de lecture de QR Code

---
+++ QRCode/Assets/Prefabs/Menu_Buttons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Buttons : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject LevelSelectPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuPanel.SetActive(true);
+        LevelSelectPanel.SetActive(false);
+    }
+
+    public void ShowLevelPanel()
+    {
+        MenuPanel.SetActive(false);
+        LevelSelectPanel.SetActive(true);
+    }
+
+    public void ShowMenuPanel()
+    {
+        MenuPanel.SetActive(true);
+        LevelSelectPanel.SetActive(false);
+    }
+}
---
+++ QRCode/Assets/Scripts/ScriptPouet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPouet : MonoBehaviour
+{
+    public void voyonsvoir()
+    {
+        Debug.Log("moins cent points pour Griffondor");
+    }
+}
