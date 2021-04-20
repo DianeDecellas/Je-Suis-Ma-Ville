09cfc6d7de863a3893d68c3d31677cbe609f8102
GuillaumeCx <guillaume.caucheteux@hotmail.fr>
2021-04-20 16:23:40 +0200

    Test du changement de fenêtre de QR code

--- QRCode/Assets/Scripts/QRCode.cs
+++ QRCode/Assets/Scripts/QRCode.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using ZXing;
 using ZXing.QrCode;
-
+/*
 public class QRCode : MonoBehaviour
 {
     public MessageDisplay fenetre;
     private WebCamTexture camTexture;
     private Rect screenRect;
+    private Rect cameraRect;
     void Start()
     {
-        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        screenRect = new Rect(0, 0, Screen.width, Screen.height); //le rectangle qui contient le retour caméra, défini par son origine (0,0) et sa largeur et sa hauteur
+        //cameraRect = new Rect(Screen.width / 4, 0, Screen.width / 2, Screen.height);
+        cameraRect = new Rect(-500, -10, 2*Screen.width, Screen.height);
         camTexture = new WebCamTexture();
         camTexture.requestedHeight = Screen.height;
         camTexture.requestedWidth = Screen.width;
@@ -24,7 +27,8 @@ public class QRCode : MonoBehaviour
     void OnGUI()
     {
         // drawing the camera on screen
-        GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+        //GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit); 
+        GUI.DrawTexture(cameraRect, camTexture, ScaleMode.ScaleToFit);
         // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
         try
         {
@@ -62,5 +66,19 @@ public class QRCode : MonoBehaviour
         encoded.SetPixels32(color32);
         encoded.Apply();
         return encoded;
+    }
+}
+*/
+
+public class QRCode : MonoBehaviour
+{
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 500, 500), "I am a button"))
+        {
+            print("You clicked the button!");
+        }
     }
 }
