c679888da6cd1aa8cc16cd99f687134b20a7d9c6
GuillaumeCx <guillaume.caucheteux@hotmail.fr>
2021-04-22 16:28:47 +0200

    Le QR Code fonctionne, et on a les commentaires à plus. Il faut maintenant rajouter la lecture du XML pour fournir le message à trouver dans le QRCode

--- unity_projet/Assets/Scripts/QRCode/QRCodeReader.cs
+++ unity_projet/Assets/Scripts/QRCode/QRCodeReader.cs
@@ -7,13 +7,14 @@ using ZXing.QrCode;
 public class QRCodeReader : MonoBehaviour
 {
     public string expectedMessage; // (G) La solution attendue du QRCode
-    public bool qrcodeValide = false;
+    public bool qrcodeValide = false; // (G) le booléen que le script XmlReader utilise pour savoir si le qrCode scanné correspond au message attendu
     private WebCamTexture camTexture;
     private Rect screenRect; //(G) le rectangle qui contiendra la caméra.   
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("QRCodeReader : Start");
         screenRect = new Rect(0, 0, Screen.width, Screen.height); //(G) rectangle défini par son origine (0,0), sa largeur et sa hauteur
         camTexture = new WebCamTexture(); //(G) La caméra
         camTexture.requestedHeight = Screen.height;
@@ -36,12 +37,11 @@ public class QRCodeReader : MonoBehaviour
             var result = barcodeReader.Decode(camTexture.GetPixels32(), camTexture.width, camTexture.height); //(G) le résultat du QR Code
             if (result != null) //(G) on traite ici le résultat lu
             {
-                Debug.Log("DECODED TEXT FROM QR: " +result.Text);
+                Debug.Log("QRCodeReader : DECODED TEXT FROM QR: " +result.Text);
                 if (result.Text == expectedMessage)
                 {
-                    Debug.Log("True");
+                    Debug.Log("QRCodeReader : True");
                     qrcodeValide = true;
-                    //(G) Ici, on pourra rajouter l'appel à une méthode de XMLreader permettant à XMLreader de valider l'étape QRCode;
                 }
             }
         }
