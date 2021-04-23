95aac1fb079f78dca6914e70d8597fa8d41691a3
DianeDecellas <diane.decellas@telecom-sudparis.eu>
2021-04-23 14:15:25 +0200

    ajout code GPS, donne accès aux coords de l utilisateur + code updatePositionText, affiche coords

---
+++ jsmv_diane_tests/Assets/Scripts/scripts cs/GPS.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//ce code sert à obtenir les coordonnées GPS latitude et longitude de l utilisateur
+public class   GPS : MonoBehaviour
+{
+
+    public static GPS Instance { set; get; }
+
+    public float latitude;
+    public float longitude;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled location");
+            yield break;
+        }
+
+        Input.location.Start();
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+
+        yield break;
+    }
+}
---
+++ jsmv_diane_tests/Assets/Scripts/scripts cs/updatePositionText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//ce code sert à afficher les coordonnées GPS utilisateur
+
+public class updatePositionText : MonoBehaviour
+{
+    public Text position;
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        position.text = "Lat:" + GPS.Instance.latitude.ToString()+"\nLon:"+ GPS.Instance.longitude.ToString();
+    }
+}
