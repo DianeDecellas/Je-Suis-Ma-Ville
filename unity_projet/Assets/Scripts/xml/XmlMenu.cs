9b11bbf30fd966a4367c795ff5f50df4dc5e688d
Oliveoly <75611130+Oliveoly@users.noreply.github.com>
2021-06-07 15:51:28 +0200

    ajout de menu déroulant avec le résumé de la balade

--- unity_projet/Assets/Scripts/xml/XmlMenu.cs
+++ unity_projet/Assets/Scripts/xml/XmlMenu.cs
@@ -12,15 +12,18 @@ using UnityEngine.SceneManagement;
 
 public class XmlMenu : MonoBehaviour
 {
-    string UrlMenu = "https://admiring-easley-a2c27c.netlify.app/MenuTest.xml"; //Url où je conserve le menu
+    string UrlMenu = "https://prefigurations.com/je_suis_ma_ville/balades/MenuTest.xml";
+    //string UrlMenu = "https://admiring-easley-a2c27c.netlify.app/MenuTest.xml"; //Url où je conserve le menu
     UrlStorage urlstorage;                                                                           // Start is called before the first frame update
     void readXmlMenu(XmlNode menu)
     {
         GameObject g;
+        int i = 0;
         XmlNode currentBalade = menu.FirstChild;
         GameObject baladeTemplate = transform.Find("baladeChoiceTemplate").gameObject;
         while (currentBalade != null)
         {
+            i++;
             XmlNode title = currentBalade.FirstChild;
             XmlNode duration = title.NextSibling;
             string url = duration.NextSibling.InnerText;
@@ -30,13 +33,38 @@ public class XmlMenu : MonoBehaviour
             g.transform.GetChild(1).GetComponent<Text>().text = duration.InnerText;
             Debug.Log("Tite - Outer = " + title.OuterXml);
             Debug.Log("Name =" + title.Name);
+            GameObject curg = g.transform.parent.GetChild(i).gameObject;
+            void displayBalade()
+            {
+                XmlDocument displayXML = new XmlDocument(); ///on crée un nouveau doc xml nommé baladeData
+                WWW dataDisplay = new WWW(url); ///oui cette fonction est obsolète mais j'ai du mal avec la nouvelle
+                while (!dataDisplay.isDone)
+                {
+                    ///cette boucle while sert à attendre qu'on ait bien toutes les données, sinon on risque d'avoir des erreurs
+                }
+
+                displayXML.LoadXml(dataDisplay.text); ///on charge le texte de data dans le doc xml baladeData
+
+                XmlElement root = displayXML.DocumentElement;   //Get follow node
+                Debug.Log("The root element is:" + root.Name);
+                XmlNode descriptif = root.FirstChild;
+                XmlNode resume = descriptif.SelectSingleNode("resume");
+                Debug.Log(resume.InnerText);
+                
+                RectTransform Rt = curg.GetComponent<RectTransform>();
+                Rt.sizeDelta = new Vector2(Rt.sizeDelta.x, Rt.sizeDelta.y + 200f);
+                
+                curg.transform.GetChild(2).GetComponent<Text>().text=resume.InnerText;
+                curg.transform.GetChild(2).gameObject.SetActive(true);
+                curg.GetComponent<Button>().onClick.AddListener(loadBalade);
+            }
             void loadBalade()
             {
                 UrlStorage.url = url;
                 Debug.Log(url);
                 SceneManager.LoadScene("scene_finale");
             }
-            g.GetComponent<Button>().onClick.AddListener(loadBalade);
+            curg.GetComponent<Button>().onClick.AddListener(displayBalade);
         }
         baladeTemplate.SetActive(false);
     }
