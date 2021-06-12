b88e24a30fcba7f2db2b857298201df16588457c
DianeDecellas <diane.decellas@telecom-sudparis.eu>
2021-06-12 19:52:55 +0200

    les feedbacks fonctionnent avec la methode get

--- unity_projet/Assets/Scripts/feedbacks.cs
+++ unity_projet/Assets/Scripts/feedbacks.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.Networking;
 
 public class feedbacks : MonoBehaviour
 {
-    
+    string FTPUrl = "https://prefigurations.com/je_suis_ma_ville/stat/statistiquesJeSuisMaVille.php";
     private float time;
     private float curTime;
+    private int note;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +25,7 @@ public class feedbacks : MonoBehaviour
 
         //here we collect at what time the walk has ended
         //string hour = dt.Hour + ":"+ dt.Minute + ":"+dt.Second;
-        string hour = dt.ToString("HH:mm:ss");
+        string hour = dt.ToString("HH:mm");
         Debug.Log(hour);
 
         //here we collect the name of the walk
@@ -54,19 +56,55 @@ public class feedbacks : MonoBehaviour
         return duree;
     }
 
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
     void donnerNote()
     {
+        //we collect information about the user's walk
+        //here we collect the date of the walk
+        DateTime dt = DateTime.Now;
+        //string date = dt.Day + "/" + dt.Month + "/" + dt.Year;
+        string date = dt.ToString("dd/MM/yyyy");
+
+        //here we collect at what time the walk has ended
+        //string hour = dt.Hour + ":"+ dt.Minute + ":"+dt.Second;
+        string hour = dt.ToString("HH:mm:ss");
+
+        //here we collect the name of the walk
+        string nom = UrlStorage.idBalade;
+
+        //here we collect the duration of the walk
+        curTime = time = (int)Time.time;
+        time = curTime - UrlStorage.time;
+        string duree = texteDuree(time);
+
+        //we prepare the request
+        string request = FTPUrl+"?"+"date="+date+"&"+"heure="+hour+"&"+"nom="+nom+"&"+"duree="+duree;
+        
+        //we want to collect the grade
         //we instantiate each button. Each button has a corresponding number
         GameObject button1 = transform.Find("Button1").gameObject;
         GameObject button2 = transform.Find("Button2").gameObject;
         GameObject button3 = transform.Find("Button3").gameObject;
         GameObject button4 = transform.Find("Button4").gameObject;
         GameObject button5 = transform.Find("Button5").gameObject;
-
-       
-        
-        //we define a default grade
-        //UrlStorage.note = 3;
         
         /*we define each button's behaviour. When the user clicks on a button, 
          * the corresponding number of stars are switched on. 
@@ -79,7 +117,10 @@ public class feedbacks : MonoBehaviour
             button3.GetComponent<changeButton>().setBool(false);
             button4.GetComponent<changeButton>().setBool(false);
             button5.GetComponent<changeButton>().setBool(false);
-            UrlStorage.note = 1;
+            note = 1;
+            request = request + "&"+"note="+note;
+            StartCoroutine(GetRequest(request));
+
         }
         button1.GetComponent<Button>().onClick.AddListener(click1);
 
@@ -90,7 +131,10 @@ public class feedbacks : MonoBehaviour
             button3.GetComponent<changeButton>().setBool(false);
             button4.GetComponent<changeButton>().setBool(false);
             button5.GetComponent<changeButton>().setBool(false);
-            UrlStorage.note = 2;
+            note = 2;
+            request = request + "&" + "note=" + note;
+            StartCoroutine(GetRequest(request));
+
         }
         button2.GetComponent<Button>().onClick.AddListener(click2);
 
@@ -101,7 +145,10 @@ public class feedbacks : MonoBehaviour
             button3.GetComponent<changeButton>().setBool(true);
             button4.GetComponent<changeButton>().setBool(false);
             button5.GetComponent<changeButton>().setBool(false);
-            UrlStorage.note = 3;
+            note = 3;
+            request = request + "&" + "note=" + note;
+            StartCoroutine(GetRequest(request));
+
         }
         button3.GetComponent<Button>().onClick.AddListener(click3);
 
@@ -112,7 +159,9 @@ public class feedbacks : MonoBehaviour
             button3.GetComponent<changeButton>().setBool(true);
             button4.GetComponent<changeButton>().setBool(true);
             button5.GetComponent<changeButton>().setBool(false);
-            UrlStorage.note = 4;
+            note = 4;
+            request = request + "&" + "note=" + note;
+            StartCoroutine(GetRequest(request));
         }
         button4.GetComponent<Button>().onClick.AddListener(click4);
 
@@ -123,7 +172,10 @@ public class feedbacks : MonoBehaviour
             button3.GetComponent<changeButton>().setBool(true);
             button4.GetComponent<changeButton>().setBool(true);
             button5.GetComponent<changeButton>().setBool(true);
-            UrlStorage.note = 5;
+            note = 5;
+            request = request + "&" + "note=" + note;
+            StartCoroutine(GetRequest(request));
+
         }
         button5.GetComponent<Button>().onClick.AddListener(click5);
 
